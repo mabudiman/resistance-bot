127b2451fb808ee76c04cce2d58a4e2220c28fe8
mabudiman <ma48ud2@gmail.com>
2017-08-19 15:45:45 +0700

    test

--- public/callback.php
+++ public/callback.php
@@ -1,6 +1,6 @@
 <?php // callback.php
-	define("LINE_MESSAGING_API_CHANNEL_SECRET", '2769fe5468f35c273fe0e634c244d519');
-	define("LINE_MESSAGING_API_CHANNEL_TOKEN", '+VU9EsuQLmIfS9AYpbE5ZTQHag1D3zIVOZe818fWb1GYYk2Pg1MsXQZq72S3McWaIv+2BF5BPeQ9aud8Abwysg7mn/Rus8ASy9ABjH3H8OkyqE1xCtlaBjbnYxjPVPX+1pQk4HdLvlDVJJ5xfnkMngdB04t89/1O/w1cDnyilFU=');
+	define("LINE_MESSAGING_API_CHANNEL_SECRET", 'ab1e0cf37c4d7d26083990ec7f1b96bf');
+	define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'LmRTYD8yrTt7RIvIsqNjZ1z+jgxHkAkSAS1qNNyjpBMNtst9dCEMIVVrM1qVVkNt3+UW04I+9WNJwZU5qVLvu2TZmslUccJU5vnnkFGEFdA5SaswFb2/qwR+OrjPazyv90uyXImmLuoSmTvCRWOn+wdB04t89/1O/w1cDnyilFU=');
 
 	require __DIR__."/../vendor/autoload.php";
 
@@ -14,25 +14,25 @@
 
 	$events = $bot->parseEventRequest($body, $signature);
 
-	$image = "failed";
+	// $image = "failed";
 
-	try {
-		$image = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder(
-			"https://resistance-bot-line-indo.herokuapp.com/images/ally-1-en.jpg",
-			"https://resistance-bot-line-indo.herokuapp.com/images/ally-1-en.jpg"
-			);
-	} catch (Exception $e) {
-	    file_put_contents("php://stderr", "Error new imagebuilder");
-	}
+	// try {
+	// 	$image = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder(
+	// 		"https://resistance-bot-line-indo.herokuapp.com/images/ally-1-en.jpg",
+	// 		"https://resistance-bot-line-indo.herokuapp.com/images/ally-1-en.jpg"
+	// 		);
+	// } catch (Exception $e) {
+	//     file_put_contents("php://stderr", "Error new imagebuilder");
+	// }
 
 	foreach ($events as $event) {
-		file_put_contents("php://stderr", "Test Gambar | User ID : ".$event->getUserId());
+		// file_put_contents("php://stderr", "Test Gambar | User ID : ".$event->getUserId());
 
 	    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
 	        $reply_token = $event->getReplyToken();
 	        $text = $event->getText();
-	        $bot->replyMessage($reply_token, $image);
-	        // $bot->replyMessage($reply_token, "test");
+	        // $bot->replyMessage($reply_token, $image);
+	        $bot->replyMessage($reply_token, "test");
 	    }
 	}
 		echo "OK";
