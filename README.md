208c6e77aced4d18a140e60794862fbf42e5fb07
mabudiman <ma48ud2@gmail.com>
2017-08-19 11:20:27 +0700

    composer require linecorp/line-bot-sdk

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # resistance-bot
 bot for the resistance
+
+todo :
+1. random siapa yang resistance / spy
+2. kasih tau lewat personal
+3. random urutan pemain (kasih tau urutannya kesemua)
+4. pilih ketua (nanti berurutan)
+5. ketua pilih nomor" pemain untuk ikut misi
+6. vote accept / reject
+7. vote success / fail yang ikut misi
+8. diumumin hasil misi
+9. ulang sampe beres
+
+more :
+1. liat siapa aja yang main beserta nomornya
+2. liat hasil sementar berapa success berapa fail
+3. buat akun? untuk score, leaderboard, history, dll
---
+++ public/callback.php
@@ -0,0 +1,25 @@
+<?php // callback.php
+	define("LINE_MESSAGING_API_CHANNEL_SECRET", '2769fe5468f35c273fe0e634c244d519');
+	define("LINE_MESSAGING_API_CHANNEL_TOKEN", '+VU9EsuQLmIfS9AYpbE5ZTQHag1D3zIVOZe818fWb1GYYk2Pg1MsXQZq72S3McWaIv+2BF5BPeQ9aud8Abwysg7mn/Rus8ASy9ABjH3H8OkyqE1xCtlaBjbnYxjPVPX+1pQk4HdLvlDVJJ5xfnkMngdB04t89/1O/w1cDnyilFU=');
+
+	require __DIR__."/../vendor/autoload.php";
+
+	$bot = new \LINE\LINEBot(
+	    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
+	    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
+	);
+
+	$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+	$body = file_get_contents("php://input");
+
+	$events = $bot->parseEventRequest($body, $signature);
+
+	foreach ($events as $event) {
+	    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+	        $reply_token = $event->getReplyToken();
+	        $text = $event->getText();
+	        $bot->replyText($reply_token, $text);
+	    }
+	}
+
+	echo "OK";
